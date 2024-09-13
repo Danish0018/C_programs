d5b938820a97e6d162d2b4c84077fef1092301f1
Your Name <you@example.com>
2024-09-13 12:05:12 +0530

    Sum

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    printf("Enter Value : ");
+    scanf("%d",&a);
+
+    printf("Enter Value : ");
+    scanf("%d",&b);
+
+    int sum = a+b;
+
+    printf("Sum is %d" , sum);
+
+    return 0;
+
+
+}
